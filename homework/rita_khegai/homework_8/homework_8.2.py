1da1ea1fa35d4baa0a090db0cb3822ef633d4659
Rita Khegai <margarita888khegay@gmail.com>
2024-06-02 13:49:22 +0200

    correction homework 8.2

--- homework/rita_khegai/homework_8/homework_8.2.py
+++ homework/rita_khegai/homework_8/homework_8.2.py
@@ -1,22 +1,33 @@
 # Напишите функцию-генератор, которая генерирует бесконечную последовательность чисел фибоначчи
 # Распечатайте из этого списка пятое число, двухсотое число, тысячное число, стотысячное число
 
+
 import sys
 
 
-n = 1
-m = 1
-fibonachi = [n, m]
+def progression(limit=10000001):
+    m = 1
+    n = 1
+    count = 1
+    while count < limit:
+        yield m
+        yield n
+        m += n
+        n += m
+        count += 1
 
-while len(fibonachi) < 100001:
-    n = n + m
-    m = n + m
-    fibonachi.append(n)
-    fibonachi.append(m)
 
 sys.set_int_max_str_digits(100000)
 
-print(fibonachi[5])
-print(fibonachi[200])
-print(fibonachi[1000])
-print(fibonachi[100000])
+
+meter = 1
+for number in progression(100000):
+    if meter == 5:
+        print(number)
+    elif meter == 200:
+        print(number)
+    elif meter == 1000:
+        print(number)
+    elif meter == 100000:
+        print(number)
+    meter += 1
